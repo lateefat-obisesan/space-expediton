3b8e725e761c61b0dd4ae7410d07479d534de797
Lateefat Obisesan <temilele06@gmail.com>
2026-07-02 06:44:32 -0500

    adding  properties to Artifact class

--- Artifact.cs
+++ Artifact.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Space_Expedition
+﻿namespace Space_Expedition
 {
-    internal class Artifact
+    class Artifact
     {
+        public string EncodedName { get; set; }
+        public string DecodedName { get; set; }
+        public string Planet { get; set; }
+        public string DiscoveryDate { get; set; }
+        public string Planet { get; set; }
+        public string Planet { get; set; }
+
     }
 }
--- Decoder.cs
+++ Decoder.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Space_Expedition
+﻿namespace Space_Expedition
 {
     internal class Decoder
     {
