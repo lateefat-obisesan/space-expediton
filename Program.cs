e50f714be09ecd2892696fc69cebc83d226eb3cb
Lateefat Obisesan <temilele06@gmail.com>
2026-07-01 19:41:38 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace space_expedition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
