eb1ec598fb1c7e7c38f7f3305510102813fa4ec9
Lateefat Obisesan <temilele06@gmail.com>
2026-07-02 07:18:54 -0500

    added original and mapped character arrays

--- Decoder.cs
+++ Decoder.cs
@@ -2,5 +2,16 @@
 {
     class Decoder
     {
+        private static readonly char[] originalLetters =
+        {
+            'A','B','C','D','E','F','G','H','I','J','K','L','M',
+            'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+        };
+
+        private static readonly char[] mappedLetters =
+       {
+            'H','Z','A','U','Y','E','K','G','O','T','I','R','J',
+            'V','W','N','M','F','Q','S','D','B','X','L','C','P'
+        };
     }
 }
