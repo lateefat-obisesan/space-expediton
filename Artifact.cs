35a5605a640a64bac8b5c017226304c22a12723a
Lateefat Obisesan <temilele06@gmail.com>
2026-07-02 06:48:49 -0500

    added constructor to Artifact class

--- Artifact.cs
+++ Artifact.cs
@@ -6,8 +6,17 @@
         public string DecodedName { get; set; }
         public string Planet { get; set; }
         public string DiscoveryDate { get; set; }
-        public string Planet { get; set; }
-        public string Planet { get; set; }
+        public string StorageLocation { get; set; }
+        public string Description { get; set; }
 
+        public Artifact(string encodedName, string decodedName, string planet, string discoveryDate, string storageLocation, string description)
+        {
+            EncodedName = encodedName;
+            DecodedName = decodedName;
+            Planet = planet;
+            DiscoveryDate = discoveryDate;
+            StorageLocation = storageLocation;
+            Description = description;
+        }
     }
 }
