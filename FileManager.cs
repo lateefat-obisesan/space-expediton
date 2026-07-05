efd435e0c33e17e76437e8704e67b0b15e54db16
Lateefat Obisesan <temilele06@gmail.com>
2026-07-05 18:33:34 -0500

    In the FileManager class, file reader and data validation logic is added

--- FileManager.cs
+++ FileManager.cs
@@ -1,6 +1,35 @@
 ﻿namespace Space_Expedition
 {
-    internal class FileManager
+    class FileManager
     {
+        public static Artifact[] ReadArtifacts(string fileName)
+        {
+            Artifact[] artifacts = new Artifact[0];
+            try
+            {
+                if(!File.Exists(fileName))
+                {
+                    Console.WriteLine("File not found.");
+                    return artifacts;
+                }
+                string[] lines = File.ReadAllLines(fileName);
+
+                foreach(string line in lines)
+                {
+                    //to skip empty lines
+                    if(string.IsNullOrWhiteSpace(line))
+                    {
+                        continue;
+                    }
+                    string[] parts = line.Split(',');
+
+                    if (parts.Length != 5)
+                    {
+                        Console.WriteLine("Invalid line skipped.");
+                        continue;
+                    }
+                }
+            }
+        }
     }
 }
