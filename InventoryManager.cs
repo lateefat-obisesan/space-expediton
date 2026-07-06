eb4a7c7ae2d63e258d8859a9e41974758a8fa43d
Lateefat Obisesan <temilele06@gmail.com>
2026-07-05 21:29:21 -0500

    added insertion sort method for the inventory array.

--- InventoryManager.cs
+++ InventoryManager.cs
@@ -1,6 +1,21 @@
 ﻿namespace Space_Expedition
 {
-    internal class InventoryManager
+    class InventoryManager
     {
+        public static void SortInventory(Artifact[] artifacts)
+        {
+            for(int i = 1; i < artifacts.Length; i++)
+            {
+                Artifact key = artifacts[i];
+                int j = i - 1;
+                while (j >= 0 && string.Compare(artifacts[j]. DecodedName, key.DecodedName) > 0)
+                {
+                    artifacts[j + 1] = artifacts[j];
+                    j--;
+                }
+                artifacts[j + 1] = key;
+            }
+        }
+        public 
     }
 }
