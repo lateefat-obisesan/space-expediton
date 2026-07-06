30dab1a7df41712be778f3cb3128fbc90650496b
Lateefat Obisesan <temilele06@gmail.com>
2026-07-05 21:56:40 -0500

    adding the file paths

--- Program.cs
+++ Program.cs
@@ -1,10 +1,25 @@
-﻿namespace space_expedition
+﻿using Space_Expedition;
+
+namespace space_expedition
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string initialFile = "galactic_vault.txt";
+            string outputFile = "expedition_summary.txt";
+
+            Console.WriteLine("Loading Galactic Vault inventory.");
+            Artifact[] inventory = FileManager.ReadArtifacts(initialFile);
+
+            InventoryManager.SortInventory(inventory);
+            Console.WriteLine($"Successfully loaded and sorted {inventory.Length} artifacts.\n");
+
+            bool running = true;
+            while (running)
+            {
+                Console.WriteLine();
+            }
         }
     }
 }
